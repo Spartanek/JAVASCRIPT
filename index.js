71ec615d297e7c63d5c438ee07418282fe111c77
Spartanek <vadimflash111@gmail.com>
2024-06-26 08:46:36 +0200

    progress

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const a = 8;
+// Перевірка пароля (if...else):
+// створіть блок if else який приймає пароль і перевіряє його на відповідність наступним критеріям:
+// "Довжина": Довжина не менше 8 символів
+// Вивести console.log() "Пароль прийнято" або відповідне повідомлення про помилку.
 
-console.log(a);
-
-// const- значення не змінюється (монументальне) та let
+// Алгоритм вирішення задачі:
+// 1. Створити input
+// 2. Знайти цей input
+const input = document.getElementById("password");
+// 3. Додати слухач подій
+input.addEventListener("input", functionSuper);
+function functionSuper(e) {
+  // (знайти значення input)
+  const value = input.value;
+  // 4. if else  перевірити довжину value
+  value.length >= 8
+    ? console.log("Пароль прийнято")
+    : console.log("Пароль не прийнято");
+}
