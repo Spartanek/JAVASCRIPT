3c1a44100afbd2f457f0ba98666c8e2e59db91dc
Spartanek <vadimflash111@gmail.com>
2024-10-04 20:43:30 +0200

    gg

--- index.js
+++ index.js
@@ -17,13 +17,42 @@ document.addEventListener("keydown", (event) => {
 // 2-е завдання
 // Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція.
 // Створи функцію createBoxes(amount), яка приймає 1 параметр amount — число. Функція створює стільки div, скільки вказано в amount і додає їх в div#boxes.
+// Створи функцію destroyBoxes(), яка очищає div#boxes.
+// При натисканні на кнопку Очистити, колекція елементів очищається.
 const deleteBoxes = function () {
   divForModifying.innerHTML = "";
 };
-const createBoxes = function (amount) {};
+// const createDivFunction = function () {
+//   const element = document.createElement("div");
+//
+//   element.style.backgroundColor = randomColor;
+//   divForModifying.append(element);
+// };
+const createBoxes = function (amount, callback) {
+  const numBoxes = Number(amount);
+  for (let i = 0; i < numBoxes; i++) {
+    // Кожен створений div:
+    let basicWidthandHeight = 10;
+    const element = document.createElement("div");
+    // element.style.width = basicWidthandHeight + 30;
+    // element.style.length = basicWidthandHeight + 30;
+    // Взагалі неправильно писав
+    element.style.width = `${basicWidthandHeight + 30}px`;
+    element.style.height = `${basicWidthandHeight + 30}px`;
+    element.textContent = `Box ${i + 1}`;
+    // const randomColor = Math.floor(Math.random() * (max + 1));
+    // вимагає 3 палітри r-red, g-green, b-blue
+    // Має випадковий rgb колір фону
+    const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+    element.style.backgroundColor = randomColor;
+    divForModifying.append(element);
+  }
+};
+let inputValue = 0;
 const inputValueFunction = function (event) {
-  const value = event.target.value;
-  createBoxes(value);
+  inputValue = event.target.value;
 };
 // додав клас до інпуту, думаю, це не порушує правил
 const input = document.querySelector(".input");
@@ -33,20 +62,14 @@ input.addEventListener("input", inputValueFunction);
 const divForModifying = document.getElementById("boxes");
 const createButton = document
   .querySelector('[data-action="render"]')
-  .addEventListener("click", createBoxes);
+  .addEventListener("click", () => {
+    createBoxes(inputValue);
+  });
 
 const deletingButton = document
   .querySelector('[data-action="destroy"]')
   .addEventListener("click", deleteBoxes);
 
-// При натисканні на кнопку Очистити, колекція елементів очищається.
-
-// Кожен створений div:
-
-// Має випадковий rgb колір фону
-
 // Розміри найпершого div — 30px на 30px
 
 // Кожен наступний div після першого, повинен бути ширше і вище попереднього на 10px
-
-// Створи функцію destroyBoxes(), яка очищає div#boxes.
