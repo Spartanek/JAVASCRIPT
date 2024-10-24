01deee0792a46d75c951168efa8b5fcd0ccee75a
Spartanek <vadimflash111@gmail.com>
2024-10-24 12:51:55 +0200

    gg

---
+++ src/JS/calculator.js
@@ -0,0 +1,6 @@
+const firstInput = document.querySelector('.first-number-input');
+const secondInput = document.querySelector('.second-number-input');
+
+const textResult = document.querySelector('.calculator-text-result');
+const buttons = document.querySelectorAll('.operation-button');
+const equalButton = document.querySelector('.equal-button');
