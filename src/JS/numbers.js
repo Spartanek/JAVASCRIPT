074c82da9fc790d5a92515a543372d4605daba15
Spartanek <vadimflash111@gmail.com>
2024-11-01 17:29:34 +0100

    fix

--- src/JS/numbers.js
+++ src/JS/numbers.js
@@ -3,3 +3,25 @@ const resultText = document.querySelector('.numbers-text-result');
 const input1 = document.querySelector('.number-input1');
 const input2 = document.querySelector('.number-input2');
 const input3 = document.querySelector('.number-input3');
+
+const maxingFunction = () => {
+  const value1 = Number(input1.value);
+  const value2 = Number(input2.value);
+  const value3 = Number(input3.value);
+  const ifStatement =
+    !isNaN(value1) &&
+    value1 !== '' &&
+    !isNaN(value2) &&
+    value2 !== '' &&
+    !isNaN(value3) &&
+    value3 !== '';
+  if (ifStatement) {
+    const maxValue = Math.max(value1, value2, value3);
+    return (resultText.textContent = `Максимальне значення ${maxValue}`);
+  } else {
+    return (resultText.textContent = 'помилка');
+  }
+};
+input1.addEventListener('input', maxingFunction);
+input2.addEventListener('input', maxingFunction);
+input3.addEventListener('input', maxingFunction);
